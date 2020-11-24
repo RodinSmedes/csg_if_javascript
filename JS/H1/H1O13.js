e884c2aca813016830f77687c56ad703b1063ff6
RodinSmedes <152068@csgaugustinus.nl>
2020-11-24 09:05:56 +0000

    Done

--- JS/H1/H1O13.js
+++ JS/H1/H1O13.js
@@ -14,21 +14,23 @@ function draw() {
   text("xJOS: " + round(xJOS) + " (mouseX:" + round(mouseX) + ")",10,20);
   text("yJOS: " + round(yJOS) + " (mouseY:" + round(mouseY) + ")",260,20);
   
-  xJOS = constrain(mouseX,100,450);
+  xJOS = constrain(mouseX,75,375);
+  yJOS = constrain(mouseY,75,375);
   
   scale(1);
-  translate(xJOS,225);
+  translate(xJOS,yJOS);
 
   // in de volgende regels wordt JOS getekend
 
   push();
+  scale(0.5);
   noStroke();
   fill('indianred');
   ellipse(0,0,150);
   fill('slategray');
   ellipse(-20,-30,50);
   ellipse(20,-30,50);
-  fill('white');
+  fill('red');
   ellipse(-20,-25,20,40);
   ellipse(20,-25,20,40);
   fill('orange');
@@ -39,5 +41,4 @@ function draw() {
   arc(0, 40, 80, 40, 0, PI, CHORD);
   pop();
   // einde tekenen JOS
-  
 }
