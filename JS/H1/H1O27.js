40fd5889d2775b4062e960d7c0aac4ab9b5c8568
RodinSmedes <152068@csgaugustinus.nl>
2021-01-05 08:49:51 +0000

    Opdracht 27 bezig

--- JS/H1/H1O27.js
+++ JS/H1/H1O27.js
@@ -13,13 +13,13 @@ function setup() {
 function draw() {
   background('lavender');
   fill('black');
-  breedte = width / 4;
+  breedte = width / aantal;
   text("aantal = " + aantal,10,20);  
   
   push();
   translate(0.5*breedte,150);
 
-  for (var n = 0; n < 4;n++) {
+  for (var n = 0; n < aantal;n++) {
     tekenJos(2);
     translate(breedte,0);
   }
@@ -27,8 +27,12 @@ function draw() {
   pop();
   
   if (keyIsDown(RIGHT_ARROW)) {
-    aantal++;
+    aantal+= 1;
   }   
+  
+  if (keyIsDown(LEFT_ARROW)) {
+    aantal-= 1;
+  }
 }
 
 function tekenJos(s) {
